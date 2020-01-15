5c45a021bb6e9024afdba04eb7f9975a13290800
rizwan230698 <rizwananwar230698@gmail.com>
2020-01-15 17:30:03 +0530

    Updates

--- precache-manifest.b72e6440c15c8b51cb1a53a9a8b23658.js
+++ precache-manifest.b72e6440c15c8b51cb1a53a9a8b23658.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "834b6e788e493eb93e65453b0138e0f5",
+    "revision": "bf65ee7627941c3708041050aa3fc459",
     "url": "/tic-tac-toe/index.html"
   },
   {
-    "revision": "729227aadbe7121ee954",
-    "url": "/tic-tac-toe/static/css/main.336ac04c.chunk.css"
+    "revision": "67710eb6ead55120238b",
+    "url": "/tic-tac-toe/static/css/main.7d75a068.chunk.css"
   },
   {
     "revision": "2b8c2078561d00cccee7",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tic-tac-toe/static/js/2.b2faca97.chunk.js.LICENSE"
   },
   {
-    "revision": "729227aadbe7121ee954",
-    "url": "/tic-tac-toe/static/js/main.21ac4b1e.chunk.js"
+    "revision": "67710eb6ead55120238b",
+    "url": "/tic-tac-toe/static/js/main.5f293447.chunk.js"
   },
   {
     "revision": "fdbd6fae367d142c593b",
