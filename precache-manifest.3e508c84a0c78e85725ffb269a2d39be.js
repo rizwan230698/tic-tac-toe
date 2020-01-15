86b2baf0a59c1b31d53440ad9e3939df226cbfda
rizwan230698 <rizwananwar230698@gmail.com>
2020-01-15 09:54:03 +0530

    Updates

---
+++ precache-manifest.3e508c84a0c78e85725ffb269a2d39be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c7e42aa6455e14d31a9f23d910a4588",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "d95b3635e7ba35a18db8",
+    "url": "/tictactoe/static/css/main.053c0251.chunk.css"
+  },
+  {
+    "revision": "2b8c2078561d00cccee7",
+    "url": "/tictactoe/static/js/2.b2faca97.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tictactoe/static/js/2.b2faca97.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d95b3635e7ba35a18db8",
+    "url": "/tictactoe/static/js/main.21ac4b1e.chunk.js"
+  },
+  {
+    "revision": "86bec5e4afa2f91612c6",
+    "url": "/tictactoe/static/js/runtime-main.870dcb66.js"
+  }
+]);
