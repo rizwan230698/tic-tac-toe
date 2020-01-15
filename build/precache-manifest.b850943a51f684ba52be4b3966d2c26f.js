d7f19d7629196f0fbabebe18bc3f951e6a6f7ed8
rizwan230698 <rizwananwar230698@gmail.com>
2020-01-15 17:42:18 +0530

    added functionality to try-again

--- build/precache-manifest.b850943a51f684ba52be4b3966d2c26f.js
+++ build/precache-manifest.b850943a51f684ba52be4b3966d2c26f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bf65ee7627941c3708041050aa3fc459",
+    "revision": "5bd8e062ee5b6c4cc7309c9e06e3f3a0",
     "url": "/tic-tac-toe/index.html"
   },
   {
-    "revision": "67710eb6ead55120238b",
+    "revision": "173a8b8389e49731f12d",
     "url": "/tic-tac-toe/static/css/main.7d75a068.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tic-tac-toe/static/js/2.b2faca97.chunk.js.LICENSE"
   },
   {
-    "revision": "67710eb6ead55120238b",
-    "url": "/tic-tac-toe/static/js/main.5f293447.chunk.js"
+    "revision": "173a8b8389e49731f12d",
+    "url": "/tic-tac-toe/static/js/main.2fe6f32e.chunk.js"
   },
   {
     "revision": "fdbd6fae367d142c593b",
